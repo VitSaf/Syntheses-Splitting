1122cb3c40624a09f1592cff4a0f6a99b1e56ea0
Vitaliy Safonov <safonov.vitvrn@gmail.com>
2020-06-15 12:31:33 +0300

    v0.1a

--- Splitting.py
+++ Splitting.py
@@ -32,12 +32,12 @@ def getDatetimeFromSeries(series):
 
 ROOT_DIR = './'
 #f-string expression
-#dframes = pd.concat([pd.read_excel(os.path.join(ROOT_DIR, f'Выгрузка_2_201{x}.xlsx'), sheet_name = f'201{x}') for x in ['7', '8', '9']], axis = 0).reset_index(drop = True)
+dframes = pd.concat([pd.read_excel(os.path.join(ROOT_DIR, f'Выгрузка_2_201{x}.xlsx'), sheet_name = f'201{x}') for x in ['7', '8', '9']], axis = 0).reset_index(drop = True)
 #1576803 строки и 24 столбца
-dframes = pd.read_excel(os.path.join(ROOT_DIR, 'testData.xlsx'), sheet_name = '2019')
+#dframes = pd.read_excel(os.path.join(ROOT_DIR, 'testData.xlsx'), sheet_name = '2019') #TestData
 #print(str(dframes['PSP_Measures_MR201_A.FQRC2001']))
 #print(str(dframes.iloc[[5],0]))#iloc[[запись], столбец]
-x = 0
+
 #newExcel()
 #Проходим по столбцу с растворителем, если больше 1000, то
 #считаем что синтез начался. Определяем какой реактор по росту давления. Ищем максимумы температуры и давления,
@@ -51,28 +51,76 @@ Cp = 'PSP_MR201_C.PIRSA2015' #S
 At = 'PSP_MR201_A.TRSA2013' #K
 Bt = 'PSP_MR201_B.TRSA2018' #P
 Ct = 'PSP_MR201_C.TRSA2023' #U
-#
-def getReactor(fromRow):#Определить название реактора,
-	tmp = dframes[Bp]
-	z = 10
-	while z > 0 :
-		print(tmp[z])#Почему 0 начинается с 3 строки???
-		z -= 1
-#Как переходить по строкам????
+#найти причину, по которой возвращает None
+def getReactor(fromRow):#Определяет название реактора,
+	A = dframes[Ap]
+	B = dframes[Bp]
+	C = dframes[Cp]
+	i = fromRow + 2
+	if (A[i] < 0.1) and (A[i+4] < A[i+5]): 
+		return 'А'
+	if (B[i] < 0.1) and (B[i+4] < B[i+5]):
+		return 'Б'
+	if (C[i] < 0.1) and (C[i+4] < C[i+5]):
+		return 'В'
+
+#Возвращает дату окончания синтеза по Р
+def getEndByP(fromRow, reactor):
+	if reactor == 'А':
+		p = dframes[Ap]
+	if reactor == 'Б':
+		p = dframes[Bp]
+	if reactor == 'В':
+		p = dframes[Cp]
+	row = fromRow + 20
+	if reactor is None:
+		return 'None?'
+	while True:
+		if p[row]>0.2 and p[row]>p[row -1] and p[row]>p[row+1]:
+			return getDatetimeFromSeries(dframes.iloc[[row], 0])
+		else:
+			row += 1
+
+def getEndByT(fromRow, reactor):
+	if reactor == 'А':
+		t = dframes[At]
+	if reactor == 'Б':
+		t = dframes[Bt]
+	if reactor == 'В':
+		t = dframes[Ct]
+	row = fromRow + 20
+	if reactor is None:
+		return 'None?'
+	while True:
+		if t[row]>70 and t[row]>t[row -1] and t[row]>t[row+1]:
+			return getDatetimeFromSeries(dframes.iloc[[row], 0])
+		else:
+			row += 1
+
+newExcel()	
+x = 0
 pause = 0 #Пауза между синтезами
 for i in dframes['PSP_Measures_MR201_A.FQRC2001'] :#Расходомер на растворителе
 	if i > 1000 and pause == 0:
+		start = getDatetimeFromSeries(dframes.iloc[[x], 0])
+		reactor = getReactor(x)
+		endP = getEndByP(x, reactor)
+		endT = getEndByT(x, reactor)
 		print(x)
-		print(i)
-		print(getDatetimeFromSeries(dframes.iloc[[x], 0]))#в excel строка соответствует i+2
+		#print(i)
+		#print(getEndByP(x, reactor))
+		#print(getEndByT(x, reactor))
+		#print(getReactor(x))
+		#print(getDatetimeFromSeries(dframes.iloc[[x], 0]))#в excel строка соответствует i+2
+		toExcel(reactor, start, endP, endT)
 		pause = 12
 	x += 1
 	if pause > 0 :
 		pause -= 1
-
-getReactor(5)
 #counter = 0
 #SZ = len(dframes.index)
+
+
 
 #while counter < SZ:
 	#print(str(dframes.iloc[[counter], 3]))
