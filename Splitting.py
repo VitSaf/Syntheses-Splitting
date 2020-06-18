2cb4313d2be14151d7e5594e955af2031d092848
Vitaliy Safonov <safonov.vitvrn@gmail.com>
2020-06-19 00:04:06 +0300

    2 блок и фикс ошибок, связанных с ним

--- Splitting.py
+++ Splitting.py
@@ -1,23 +1,20 @@
 import pandas as pd
-import os
-import xlwt
-import xlrd
-from xlutils.copy import copy
 import join
 from datetime import datetime, timedelta
 
 
 
-ROOT_DIR = './'
 MAIN_LIST = []
 ERRORS_LIST = []
 SYNT_START_VALUE = 30000 #753 строка 2019
 MAX_SYNT_TIME = timedelta(hours = 2)
 BLOCK1_START_VALUE_CATALYST = 300
+BLOCK2_START_VALUE_BUTADIEN = 10000
 
 #расходомер на растворителе
 F_SOLVENT = 'PSP_Measures_MR201_A.FQRC2001' #D
 F_CATALYST = 'PSP_Measures_MR201_A.FRCA1411' #G
+F_BUTADIEN = 'PSP_Measures_MR201_A.FRC1061' #E
 #Давление
 Ap = 'PSP_MR201_A.PIRSA2011' #I
 Bp = 'PSP_MR201_B.PIRSA2013' #N
@@ -90,22 +87,13 @@ def getPFromReactor(reactor):
 		return 'None?'
 
 
-#Возвращает дату окончания синтеза по Р
-def getEndByP(fromRow, reactor):
-	p = getPFromReactor(reactor)
-	row = fromRow + 20
-	while True:
-		if p[row]>0.2 and p[row]>p[row -1] and p[row]>p[row+1]:
-			return getDatetimeFromSeries(dframes.iloc[[row], 0])
-		else:
-			row += 1
-
 def getEndByT(fromRow, reactor):
 	t = getTFromReactor(reactor)
 	row = fromRow + 20
 	while True:
 		if t[row]>70 and t[row]>t[row -1] and t[row]>t[row+1]:
-			return getDatetimeFromSeries(dframes.iloc[[row], 0])
+			#return getDatetimeFromSeries(dframes.iloc[[row], 0])
+			return row
 		else:
 			row += 1
 #Вернет номер строки, с которой началась подача бутиллития 
@@ -118,6 +106,7 @@ def getFirstBlockStart(fromRow):
 
 #Вернет номер строки окончания первого блока по Р
 def getFirstBlockEndByP(fromRow, reactor):
+	fromRow += 3
 	p = getPFromReactor(reactor)
 	while True:
 		if (p[fromRow] > p[fromRow-1]) and (p[fromRow] > p[fromRow+1]): #Если это первый максимум, то возвращаем номер строки
@@ -125,17 +114,40 @@ def getFirstBlockEndByP(fromRow, reactor):
 		fromRow += 1
 
 def getFirstBlockEndByT(fromRow, reactor):
+	fromRow += 3
 	t = getTFromReactor(reactor)
 	while True:
 		if (t[fromRow] > t[fromRow-1]) and (t[fromRow] > t[fromRow+1]): #Если это первый максимум, то возвращаем номер строки
 			return fromRow
 		fromRow += 1
+#начало второго блока - конец подачи бутадиена (судя по трендам)
+def getSecondBlockStart(fromRow):
+	tmp = dframes[F_BUTADIEN]
+	fromRow += 20#задержка для избежания пересечения синтезов
+	while True:
+		if tmp[fromRow] > BLOCK2_START_VALUE_BUTADIEN:#нашли начало слива бутадиена
+			while True: #ищем конец слива бутадиена
+				if tmp[fromRow] < BLOCK2_START_VALUE_BUTADIEN:#нашли конец слива бутадиена
+					return fromRow
+				fromRow += 1
+		fromRow += 1
+
+#Возвращает дату окончания синтеза второго блока
+def getSecondBlockEnd(fromRow, reactor):
+	p = getPFromReactor(reactor)
+	row = fromRow + 20
+	while True:
+		if p[row]>0.2 and p[row]>p[row -1] and p[row]>p[row+1]:
+			#return getDatetimeFromSeries(dframes.iloc[[row], 0])
+			return row
+		else:
+			row += 1
 
 
 
 print("Start")
-#dframes = pd.read_excel(os.path.join(ROOT_DIR, 'testData.xlsx'), sheet_name = '2019') #TestData
-dframes = join.load()
+dframes = join.loadTest() #TestData
+#dframes = join.load()
 print('dataframe загружен')
 #print(str(dframes.iloc[[5],0]))#iloc[[запись], столбец]
 
@@ -144,7 +156,7 @@ print('dataframe загружен')
 x = 0
 solvent_pause = 0 #Пауза между синтезами
 for i in dframes[F_SOLVENT] :#Расходомер на растворителе
-	print(x)
+	#print(x)
 	if i > SYNT_START_VALUE and solvent_pause == 0:
 		start = getDatetimeFromSeries(dframes.iloc[[x], 0])
 		try:
@@ -154,24 +166,35 @@ for i in dframes[F_SOLVENT] :#Расходомер на растворителе
 				x += 1
 				continue
 			else:
-				#endByP = getEndByP(x, reactor)
-				block1Start = getDatetimeFromSeries(dframes.iloc[[getFirstBlockStart(x)], 0])
+				block1Start = getFirstBlockStart(x)
+				block1StartDatetime = getDatetimeFromSeries(dframes.iloc[[block1Start], 0])
 				endByT = getEndByT(x, reactor)#в excel строка соответствует i+2
-				block1EndByP = getDatetimeFromSeries(dframes.iloc[[getFirstBlockEndByP(x, reactor)], 0])
-				block1EndByT = getDatetimeFromSeries(dframes.iloc[[getFirstBlockEndByT(x, reactor)], 0])
+				endByTDatetime = getDatetimeFromSeries(dframes.iloc[[endByT], 0])
+				block1EndByP = getFirstBlockEndByP(x, reactor)
+				block1EndByT = getFirstBlockEndByT(x, reactor)
+				block1EndByPDatetime = getDatetimeFromSeries(dframes.iloc[[block1EndByP], 0])
+				block1EndByTDatetime = getDatetimeFromSeries(dframes.iloc[[block1EndByT], 0])
+				block2Start = getSecondBlockStart(x)
+				block2End = getSecondBlockEnd(x, reactor)
+				block2StartDatetime = getDatetimeFromSeries(dframes.iloc[[block2Start], 0])
+				block2EndDatetime = getDatetimeFromSeries(dframes.iloc[[block2End], 0])
 		except (IndexError, KeyError):#В LookupError входят IndexError и KeyError (оба эксепшена могут тут прокнуть)
 			x += 1
 			continue
 
+		print(reactor)
+		print(block2StartDatetime)
+		print(block1EndByPDatetime)
+		totalDuration = endByTDatetime - start
+		block1DurationByP = block1EndByPDatetime - block1StartDatetime
+		block1DurationByT = block1EndByTDatetime - block1StartDatetime
+		block2Duration = block2EndDatetime - block2StartDatetime
 
-
-		totalDuration = endByT - start
-		block1DurationByP = block1EndByP - block1Start
-		block1DurationByT = block1EndByT - block1Start
+		pauseBetweenBlock1AndBlock2 = block2StartDatetime - block1EndByPDatetime# ошибки из-за пересечения синтезов
 
 		if totalDuration < MAX_SYNT_TIME:
-			MAIN_LIST.append({'Реактор':reactor, 'Начало синтеза':str(start), 'Конец синтеза': str(endByT), 'Длительность синтеза':str(totalDuration), 'Длительность первого блока по Р': str(block1DurationByP),
-				'Длительность первого блок по Т':str(block1DurationByT)})
+			MAIN_LIST.append({'Реактор':reactor, 'Начало синтеза':str(start), 'Конец синтеза': str(endByTDatetime), 'Длительность синтеза':str(totalDuration), 'Длительность первого блока по Р': str(block1DurationByP),
+				'Длительность первого блока по Т':str(block1DurationByT), 'Длительность второго блока': str(block2Duration), 'Пауза между 1(по Р) и 2 блоком': str(pauseBetweenBlock1AndBlock2)})
 		else:
 			ERRORS_LIST.append({'Реактор':reactor, 'Начало синтеза':str(start), 'Конец синтеза': str(endByT)})
 
