b725014916378e9dade4e7ccb9ba741785fbe4ba
Vitaliy Safonov <safonov.vitvrn@gmail.com>
2020-07-21 06:57:48 +0300

    finished

--- tepAnalysys/Untitled.py
+++ tepAnalysys/Untitled.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[89]:
-
 
 import pandas as pd
 import os
@@ -17,36 +12,23 @@ df = pd.read_excel(os.path.join(ROOT_DIR, 'Копия ТЭП-50 Выгрузка
 #df.info()
 
 
-steam = df['Расход пара на 2-ю ступень дегазации MS-312, Гкал/час']
-polimer = df['Общая подача полимера на дегазацию, т/час']
+with_pumps = []
+without_pumps = []
+df.info()
+pump1 = df['PSP_Pumps_801_A_S.State1']
+pump2 = df['PSP_Pumps_801_A_S.State2']
+#pump1.replace('Null', 0)
+#pump2.replace('Null', 0)
+#pump1.astype(float)
+#pump2.astype(float)
+x = 0
+for i in pump1:
+	print(i, pump2[x], i == '1', pump2[x] == '1', i == 1, pump2[x] == 1)
+	if i == 1 or pump2[x] == 1:
+		with_pumps.append(df.iloc[x])
+	else :
+		without_pumps.append(df.iloc[x])
+	x += 1
 
-#steam = steam.astype('float64')
-steam = steam.replace(0, 0.1)
-steam = steam.dropna()
-#polimer = polimer.astype('float64')
-polimer = polimer.replace(0, np.nan)
-polimer = polimer.dropna()
-
-df['total'] = steam / polimer
-
-
-dftmp = df[28466:]#c 1.04.20
-ekons_udel = dftmp['Удельный расход, Гкал/т']
-ekons_udel = ekons_udel.replace('', np.nan)
-ekons_udel = ekons_udel.dropna()
-ekons_udel = ekons_udel.astype(float)
-
-
-steam_udel = df['Удельный расход, Гкал/т']
-steam_udel =  steam_udel.replace('', np.nan)
-steam_udel = steam_udel.dropna()
-steam_udel = steam_udel.astype(float)
-
-
-print(steam_udel.shape)# sns.distplot(steam_udel, color='red')
-# plt.show()
-
-#sns.distplot(ekons_udel, bins=np.linspace(0,3,30), hist=False, kde=True) # Через Seaborn, не работает
-(n, bins, patches) = plt.hist(steam_udel, bins=np.linspace(0,3,30))  # Через mpl
-(n, bins, patches) = plt.hist(ekons_udel, bins = np.linspace(0, 3, 30))
-plt.show()
+pd.DataFrame(with_pumps).to_excel('С увлажнением.xlsx')
+pd.DataFrame(without_pumps).to_excel('Без увлажнения.xlsx')
